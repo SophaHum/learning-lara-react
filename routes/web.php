ab934916de7dc77672ced16ceb3c9f2b718d0760
Hum Sopha <sopha.humm@gmail.com>
2024-08-20 13:05:36 +0700

    2-second-commit

--- routes/web.php
+++ routes/web.php
@@ -4,19 +4,12 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use App\Http\Controllers\BlogController;
 
-Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
+// Route::get('/component', function () {
+//     return Inertia::render('Components/components');
+// })->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -24,4 +17,12 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+});
+
+Route::get('/', function(){
+    return Inertia::render('Dashboard');
+});
+
+require __DIR__ . '/auth.php';
